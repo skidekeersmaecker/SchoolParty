92fdea54e8f308cfba392d2de78d3b8c10de90b9
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2020-06-16 22:39:32 +0200

    gallerij logica steekt er in en geluiden toegevoegd, nu enkel nog selectie an de juiste media

--- main.js
+++ main.js
@@ -1,24 +1,57 @@
+var media = [];
+var geluid;
+
 function KlikKlas(klas){
     console.log(klas);
-    document.getElementById('gallerij').style.visibility = "visible";
+    document.getElementById('gallerij').innerHTML = "<button id='btnSluitGallerij' aria-label='Sluit Gallerij' onclick='SluitGallerij()'>×</button><div id='nanogallery2'>gallery_made_with_nanogallery2</div>";
+    
+    SpeelGeluid(klas);
+
+    media = [];
+    for(i = 1; i < 9; i++){
+        //FOTO
+        if(doesFileExist('images/kunst/' + klas + '/' + klas + '-' + i + '.jpg')){
+            media.push({ src: 'images/kunst/' + klas + '/' + klas + '-' + i + '.jpg', srct: 'images/kunst/' + klas + '/' + klas + '-' + i + '.jpg', title: '' });
+        }
+        else console.log('Does not exist: ' + 'images/kunst/' + klas + '/' + klas + '-' + i + '.jpg');
+        //VIDEO
+        if(doesFileExist('images/kunst/' + klas + '/' + klas + '-' + i + '.mp4')){
+            media.push({ src: 'images/kunst/' + klas + '/' + klas + '-' + i + '.jpg', srct: 'images/kunst/' + klas + '/' + klas + '-' + i + '.jpg', title: '' });
+        }
+        else console.log('Does not exist: ' + 'images/kunst/' + klas + '/' + klas + '-' + i + '.jpg')
+    };
 
     jQuery("#nanogallery2").nanogallery2( {
         // ### gallery settings ### 
         thumbnailHeight:  300,
         thumbnailWidth:   300,
-        //itemsBaseURL:     'images/kunst' //'https://nanogallery2.nanostudio.org/samples/',
         
         // ### gallery content ### 
-        items: [
-            { src: 'images/kunst/K1A/Bandensporen maken .jpg', srct: 'images/kunst/K1A/Bandensporen maken .jpg', title: 'Bandensporen maken' },
-            { src: 'images/kunst/K1A/Onderzoekend leren -  verven met ijs en verf.jpg', srct: 'images/kunst/K1A/Onderzoekend leren -  verven met ijs en verf.jpg', title: 'verven met ijs en verf' },
-            { src: 'images/kunst/K1A/onderzoekend leren - archeoloog dino opgraven .jpg', srct: 'images/kunst/K1A/onderzoekend leren - archeoloog dino opgraven .jpg', title: 'archeoloog dino opgraven' }
-          ]
-      });
+        items: media
+    });
 
+    document.getElementById('gallerij').style.visibility = "visible";
+}
+
+function SpeelGeluid(klas){
+    geluid = new Audio('sounds/' + klas + '.mp3');
+    geluid.play();
+}
+
+function doesFileExist(urlToFile) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('HEAD', urlToFile, false);
+    xhr.send();
+     
+    if (xhr.status == "404") {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 function SluitGallerij(){
     document.getElementById('gallerij').style.visibility = "hidden";
+    document.getElementById('gallerij').innerHTML = '';
     console.log('sluit')
 }
